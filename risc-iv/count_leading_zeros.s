1670a5790d6c4c8286f10f76019f702bcf5dad7b
deadxraver <409853@edu.itmo.ru>
2025-04-30 11:50:40 +0300

    fix risc-iv

--- risc-iv/count_leading_zeros.s
+++ risc-iv/count_leading_zeros.s
@@ -1,50 +1,46 @@
   .data
 input_addr:   .word 0x80
 output_addr:  .word 0x84
-zeros_count:  .word 0x20
   .text
 
 
-count_leading_zeros:      ; t0 - number, t1 - counter, t2 - hardcoded 1 for shifts
+count_leading_zeros:      ; a0 - number, a1 - counter, t2 - hardcoded 1 for shifts
   addi      sp, sp, -4
-  sw        a0, 0(sp)
+  sw        ra, 0(sp)
   addi      t2, zero, 1
 
-  beqz      t0, count_leading_zeros_ret
-  beqz      t1, count_leading_zeros_ret
-  addi      t1, t1, -1
-  sra       t0, t0, t2
-  jal       a0, count_leading_zeros
+  beqz      a0, count_leading_zeros_ret
+  beqz      a1, count_leading_zeros_ret
+  addi      a1, a1, -1
+  sra       a0, a0, t2
+  jal       ra, count_leading_zeros
 
 count_leading_zeros_ret:
-  lw        a0, 0(sp)
+  lw        ra, 0(sp)
   addi      sp, sp, 4
-  jr        a0
+  jr        ra
 
 
 print_res:
-  addi      sp, sp, -4
-  sw        a0, 0(sp)
-
   lui       t0, %hi(output_addr)
   addi      t0, t0, %lo(output_addr)
   lw        t0, 0(t0)
-  sw        t1, 0(t0)
-  lw        a0, 0(sp)
-  addi      sp, sp, 4
-  jr        a0
+  sw        a0, 0(t0)
+  jr        ra
 
 
 _start:
+  addi      sp, zero, 0x120
   lui       t0, %hi(input_addr)
   addi      t0, t0, %lo(input_addr)
   lw        t0, 0(t0)
 
-  lw        t0, 0(t0)                       ; load value from input
-  addi      t1, zero, 0x20                  ; zeros counter
+  lw        a0, 0(t0)                       ; load value from input
+  addi      a1, zero, 0x20                  ; zeros counter
 
-  jal       a0, count_leading_zeros
-  jal       a0, print_res
+  jal       ra, count_leading_zeros
+  mv        a0, a1
+  jal       ra, print_res
   halt
 
 
