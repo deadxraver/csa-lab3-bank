ee6218985c5f5eacabfd85836a833a802fcef774
deadxraver <409853@edu.itmo.ru>
2025-09-13 21:11:17 +0300

    replace input_streams with mmio in configs

--- f32a/hello_user_cstr.s
+++ f32a/hello_user_cstr.s
@@ -18,24 +18,24 @@ overflow_error:
   halt
 
 sub:
-  inv lit 1 + +
+  inv lit 1 + +           \; -(stack.pop()) + stack.pop()
   ;
 
 read_name:
-  lit buffer a!         \; a = &buffer
-  @p input_addr b!      \; b = stdin
+  lit buffer a!           \; a = &buffer
+  @p input_addr b!        \; b = stdin
 read_name_loop:
-  a lit buffer_end sub  \; ptr - &buffer_end
+  a lit buffer_end sub    \; ptr - &buffer_end
   -if overflow_error
-  @b dup @p endline xor \; cmp c with endline
+  @b dup @p endline xor   \; cmp c with endline
   if read_name_end
-  @ lit 0xffff_ff00 and +
+  @ lit 0xffff_ff00 and + \; get upper 3 bytes of a word
   !+
-  read_name_loop ;
+  read_name_loop ;        \; next iteration
 read_name_end:
-  drop
-  a lit buffer xor if overflow_error
-  @ lit 0xffff_ff00 and !
+  drop                    \; drop endline
+  a lit buffer xor if overflow_error  \; check if input is not empty
+  @ lit 0xffff_ff00 and ! \; get upper bytes
   lit buffer a!
 put_exclamation_loop:
   @+ @p byte_mask and
